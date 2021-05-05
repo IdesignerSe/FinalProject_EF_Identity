48344f5c7249683ecfe01c433e20d545bb9e152a
idesignerse <heberandrade@gmail.com>
2021-05-05 09:14:10 +0200

    Massive Code
    This version has EF and Identity  using in sqlServer

---
+++ Models/AdvancedUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MVC_Habit
+{
+    public class AdvancedUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public int Age { get; set; }
+        public int MobileNumber { get; set; }
+        public string Country { get; set; }
+        public string Email2 { get; set; }
+        public string PostCode { get; set; }
+    }
+}
---
+++ Models/HabitContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_Habit.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace MVC_Habit.Models
+{
+    public class HabitContext : IdentityDbContext<AdvancedUser>
+    {
+        public HabitContext(DbContextOptions<HabitContext> contextOptions) : base(contextOptions)
+        {
+        }
+
+        public DbSet<Habit> Habits { get; set; }
+
+        public DbSet<MVC_Habit.Models.ProgramSet> ProgramSet { get; set; }
+
+        public DbSet<MVC_Habit.Models.Article> Article { get; set; }
+    }
+}
+//Saving to Github
